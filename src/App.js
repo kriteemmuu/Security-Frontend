bed51fa75062565d1d683072e9d9451bb51750b0
kriteemmuu <kritimakhatri123@gmail.com>
2025-02-01 10:16:24 +0545

    fixed

--- src/App.js
+++ src/App.js
@@ -1,47 +1,60 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import React, { lazy, Suspense, useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useLocation,
 } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+
+// Lazy-loaded components
 const Home = lazy(() => import("./pages/homepage/Home.js"));
-import TopHeader from "./components/topHeader/TopHeader.js";
-import Login from "./pages/login/Login.js";
-import Register from "./pages/register/Register.js";
-import { ToastContainer } from "react-toastify";
+const VerifyAccount =lazy(()=>import("./components/verifyAccount/VerifyAccount.js"))
 const ProfilePage = lazy(() => import("./pages/profile/ProfilePage.jsx"));
 const ProductDetails = lazy(() => import("./pages/homepage/ProductDetails.js"));
 const Product = lazy(() => import("./pages/product/Product.jsx"));
-import WishList from "./pages/cart/cartDetails/WishList.jsx";
-import UpdateProduct from "./pages/product/UpdateProduct.jsx";
-import CartDetails from "./pages/cart/cartDetails/CartDetails.jsx";
-import Search from "./pages/homepage/Search.jsx";
-import ContactUs from "./pages/homepage/ContactUs.jsx";
-import Checkout from "./pages/cart/cartDetails/Checkout.jsx";
-import Khalti from "./components/khalti/Khalti.js";
-import Review from "./pages/homepage/Review.jsx";
-import MainLayout from "./components/admin/mainLayout/MainLayout.jsx";
-import AdminProductList from "./components/admin/productsList/AdminProductList.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import AllUserList from "./components/admin/userList/AllUserList.jsx";
-import { lazy, Suspense, useEffect, useState } from "react";
-import SkeletonLoading from "./components/layout/skeletonLoading/SkeletonLoading.jsx";
-import PrivateRoute from "./components/layout/privateRoute/PrivateRoute.jsx";
-import DashBoard from "./components/admin/dashboardGrid/DashBoard.jsx";
-import SingleUserData from "./components/admin/singleUserData/SingleUserData.jsx";
-import OrderSuccess from "./components/orderSuccess/OrderSuccess.jsx";
-import PageNotFound from "./components/pageNotFound/PageNotFound.jsx";
-import OrderHistory from "./components/orderHistory/OrderHistory.jsx";
-import SingleOrderHistory from "./components/singleOrderHistory/SingleOrderHistory.jsx";
-import AdminOrderList from "./components/admin/adminOrderList/AdminOrderList.jsx";
-import AdminSingleOrder from "./components/admin/adminOrderList/adminSingleOrder/AdminSingleOrder.jsx";
+const TopHeader = lazy(() => import("./components/topHeader/TopHeader.js"));
+const Login =lazy(()=>import("./pages/login/Login.js"));
+const Register = lazy(() => import("./pages/register/Register.js"));
+const WishList = lazy(() => import("./pages/cart/cartDetails/WishList.jsx"));
+const UpdateProduct = lazy(() => import("./pages/product/UpdateProduct.jsx"));
+const CartDetails = lazy(() => import("./pages/cart/cartDetails/CartDetails.jsx"));
+const Search = lazy(() => import("./pages/homepage/Search.jsx"));
+const ContactUs = lazy(() => import("./pages/homepage/ContactUs.jsx"));
+const Checkout = lazy(() => import("./pages/cart/cartDetails/Checkout.jsx"));
+const Khalti = lazy(() => import("./components/khalti/Khalti.js"));
+const Review = lazy(() => import("./pages/homepage/Review.jsx"));
+const MainLayout = lazy(() => import("./components/admin/mainLayout/MainLayout.jsx"));
+const AdminProductList = lazy(() => import("./components/admin/productsList/AdminProductList.jsx"));
+const Footer = lazy(() => import("./components/footer/Footer.jsx"));
+const AllUserList = lazy(() => import("./components/admin/userList/AllUserList.jsx"));
+const SkeletonLoading = lazy(() => import("./components/layout/skeletonLoading/SkeletonLoading.jsx"));
+const PrivateRoute = lazy(() => import("./components/layout/privateRoute/PrivateRoute.jsx"));
+const DashBoard = lazy(() => import("./components/admin/dashboardGrid/DashBoard.jsx"));
+const SingleUserData = lazy(() => import("./components/admin/singleUserData/SingleUserData.jsx"));
+const OrderSuccess = lazy(() => import("./components/orderSuccess/OrderSuccess.jsx"));
+const PageNotFound = lazy(() => import("./components/pageNotFound/PageNotFound.jsx"));
+const OrderHistory = lazy(() => import("./components/orderHistory/OrderHistory.jsx"));
+const SingleOrderHistory = lazy(() => import("./components/singleOrderHistory/SingleOrderHistory.jsx"));
+const AdminOrderList = lazy(() => import("./components/admin/adminOrderList/AdminOrderList.jsx"));
+const AdminSingleOrder = lazy(() => import("./components/admin/adminOrderList/adminSingleOrder/AdminSingleOrder.jsx"));
+const AboutUs = lazy(() => import("./pages/findUs/AboutUs.jsx"));
+const PrivacyPolicy = lazy(() => import("./pages/findUs/PrivacyPolicy.jsx"));
+const LegalNotice = lazy(() => import("./pages/findUs/LegalNotice.jsx"));
+const TermsAndConditions = lazy(() => import("./pages/findUs/TermsAndConditions.jsx"));
+
 
 const AppContent = () => {
   const location = useLocation();
   const isAdminRoute = location.pathname.startsWith("/admin_dashboard");
-  const user = JSON.parse(localStorage.getItem("user"));
+  const user = localStorage.getItem("user") 
+  ? JSON.parse(localStorage.getItem("user")) 
+  : {};
+
 
   const [cartItemsCount, setCartItemsCount] = useState(0);
 
@@ -78,6 +91,7 @@ const AppContent = () => {
           <Route path="/cart-details" element={<CartDetails />} />
           <Route path="/wishlist" element={<WishList />} />
 
+
           {/* //AdminPanel */}
           <Route
             path="/admin_dashboard/*"
@@ -104,6 +118,8 @@ const AppContent = () => {
           </Route>
 
           <Route path="/search" element={<Search />} />
+          {/* <Route path="/verify-account" element={<VerifyAccount />} /> */}
+
           <Route path="*" element={<PageNotFound />} />
           <Route path="/order-success" element={<OrderSuccess />} />
           <Route
@@ -132,7 +148,13 @@ const AppContent = () => {
             }
           />
           <Route path="/khalti" element={<Khalti />} />
+          <Route path="/verify-account" element={<VerifyAccount />} />
+
           <Route path="/review/:productId" element={<Review />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/legal-notice" element={<LegalNotice />} />
+          <Route path="/terms-conditions" element={<TermsAndConditions />} />       
         </Routes>
       </Suspense>
       {!isAdminRoute && <Footer />}
